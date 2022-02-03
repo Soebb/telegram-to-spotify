f12eb92e9ae13aa60902c74d29074cf070846268
viktor <dangerouzua@gmail.com>
2022-02-03 22:38:09 +0200

    fixes in telegram part

--- tg.py
+++ tg.py
@@ -1,6 +1,6 @@
 from telethon.sync import TelegramClient
-from telethon.tl.types import InputMessagesFilterPhotos
 import os
+import re
 from datetime import datetime, timedelta
 
 
@@ -30,7 +30,7 @@ else:
     monday = (dt - timedelta(days=dt.weekday())).strftime("%Y-%m-%d")
     m1 = datetime.today().date()
     tuesday = (mnd + timedelta(days=1)).strftime("%Y-%m-%d")
-    t1 =datetime.today().date() + timedelta(days=1)
+    t1 = datetime.today().date() + timedelta(days=1)
     wednesday = (mnd + timedelta(days=2)).strftime("%Y-%m-%d")
     w1 = datetime.today().date() + timedelta(days=2)
     thursday = (mnd + timedelta(days=3)).strftime("%Y-%m-%d")
@@ -42,26 +42,42 @@ else:
     sunday = (mnd + timedelta(days=6)).strftime("%Y-%m-%d")
     sa1 = datetime.today().date() + timedelta(days=6)
 
-
 with TelegramClient('session_name', int(api_id), api_hash) as client:
     # monday
+    print('monday')
     for message in client.iter_messages(chat, offset_date=m1, search=f"Release Date: {monday}"):
-        print(message.sender_id, ':', message.text)
+        print(message.text)
+        re.findall('\[(.*?)\]',message.test)
 
+    print('tuesday')
     for message in client.iter_messages(chat, offset_date=t1, search=f"Release Date: {tuesday}"):
-        print(message.sender_id, ':', message.text)
+        print(message.text)
+        re.findall('\[(.*?)\]', message.test)
 
+    print('wednesday')
     for message in client.iter_messages(chat, offset_date=w1, search=f"Release Date: {wednesday}"):
-        print(message.sender_id, ':', message.text)
+        print(message.text)
+        re.findall('\[(.*?)\]', message.test)
 
+    print('thursday')
     for message in client.iter_messages(chat, offset_date=th1, search=f"Release Date: {thursday}"):
-        print(message.sender_id, ':', message.text)
+        print(message.text)
+        re.findall('\[(.*?)\]', message.test)
 
+    print('friday')
     for message in client.iter_messages(chat, offset_date=f1, search=f"Release Date: {friday}"):
-        print(message.sender_id, ':', message.text)
+        print(message.text)
+        re.findall('\[(.*?)\]', message.test)
 
+    print('saturday')
     for message in client.iter_messages(chat, offset_date=s1, search=f"Release Date: {saturday}"):
-        print(message.sender_id, ':', message.text)
+        print(message.text)
+        re.findall('\[(.*?)\]', message.test)
 
+    print('sunday')
     for message in client.iter_messages(chat, offset_date=sa1, search=f"Release Date: {sunday}"):
-        print(message.sender_id, ':', message.text)
+        print(message.text)
+        re.findall('\[(.*?)\]', message.test)
+
+client.start()
+client.run_until_disconnected()
